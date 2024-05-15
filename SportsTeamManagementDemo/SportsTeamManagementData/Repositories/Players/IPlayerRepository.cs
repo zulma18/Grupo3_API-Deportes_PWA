28446a6810108088711e661d7acbdda4c9a57a2a
KevinU20221275 <U20221275@univo.edu.sv>
2024-05-15 12:41:11 -0600

    added player Repository

---
+++ SportsTeamManagementDemo/SportsTeamManagementData/Repositories/Players/IPlayerRepository.cs
@@ -0,0 +1,13 @@
+﻿using SportsTeamManagementData.Models;
+
+namespace SportsTeamManagementData.Repositories.Players
+{
+    public interface IPlayerRepository
+    {
+        Task AddPlayerAsync(Player player);
+        Task DeletePlayerAsync(int id);
+        Task EditPlayerAsync(Player player);
+        Task<Player?> GetPlayerByIdAsync(int id);
+        Task<IEnumerable<Player>> GetPlayersAsync();
+    }
+}
---
+++ SportsTeamManagementDemo/SportsTeamManagementData/Repositories/Players/PlayerRepository.cs
@@ -0,0 +1,58 @@
+﻿using SportsTeamManagementData.Data;
+using SportsTeamManagementData.Models;
+
+
+namespace SportsTeamManagementData.Repositories.Players
+{
+    public class PlayerRepository : IPlayerRepository
+    {
+        private readonly IDbDataAccess _dataAccess;
+
+        public PlayerRepository(IDbDataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+
+        public async Task<IEnumerable<Player>> GetPlayersAsync()
+        {
+            return await _dataAccess.GetDataAsync<Player, dynamic>(
+                "dbo.spPlayer_GetAll",
+                new { }
+                );
+        }
+
+        public async Task<Player?> GetPlayerByIdAsync(int id)
+        {
+            var product = await _dataAccess.GetDataAsync<Player, dynamic>(
+                "dbo.spPlayer_GetById",
+                new { Id = id }
+                );
+
+            return product.FirstOrDefault();
+        }
+
+        public async Task AddPlayerAsync(Player player)
+        {
+            await _dataAccess.SaveDataAsync(
+                "dbo.spPlayer_Insert",
+                new { player.FirstName, player.LastName, player.Age, player.TeamId }
+                );
+        }
+
+        public async Task EditPlayerAsync(Player player)
+        {
+            await _dataAccess.SaveDataAsync(
+                "dbo.spPlayer_Update",
+                new { player.Id, player.FirstName, player.LastName, player.Age, player.TeamId }
+                );
+        }
+
+        public async Task DeletePlayerAsync(int id)
+        {
+            await _dataAccess.SaveDataAsync(
+                "dbo.spPlayer_Delete",
+                new { Id = id }
+                );
+        }
+    }
+}
