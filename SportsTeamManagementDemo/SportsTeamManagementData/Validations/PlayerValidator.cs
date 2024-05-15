44f8575861a2c83320eb8eb28c9239b571cad927
KevinU20221275 <U20221275@univo.edu.sv>
2024-05-15 12:41:42 -0600

    added validations for player model

---
+++ SportsTeamManagementDemo/SportsTeamManagementData/Validations/PlayerValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using SportsTeamManagementData.Models;
+
+namespace SportsTeamManagementData.Validations
+{
+    public class PlayerValidator : AbstractValidator<Player>
+    {
+        public PlayerValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("El nombre es Obligatorio")
+                .MinimumLength(2).WithMessage("Debe contener minimo 3 letras")
+                .MaximumLength(75).WithMessage("Solo se permite un maximo de 75 caracteres");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .WithMessage("El Apellido es Obligatorio")
+                .MinimumLength(2).WithMessage("Debe contener minimo 2 letras")
+                .MaximumLength(75).WithMessage("Solo se permite un maximo de 75 caracteres");
+
+            RuleFor(x => x.Age)
+                .NotEmpty()
+                .WithMessage("La edad es Obligatoria")
+                .GreaterThan(6).WithMessage("La edad minima para contratar jugadores es 7 Años");
+
+            RuleFor(x => x.TeamId)
+                .NotEmpty()
+                .WithMessage("El Equipo es Obligatorio");
+        }
+    }
+}
