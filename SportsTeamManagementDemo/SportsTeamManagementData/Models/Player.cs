5175c3b9bf36f70c1e228843c3e278f5657d2378
KevinU20221275 <U20221275@univo.edu.sv>
2024-05-15 12:39:29 -0600

    added player model

---
+++ SportsTeamManagementDemo/SportsTeamManagementData/Models/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsTeamManagementData.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public required string FirstName { get; set; }
+
+        [Required]
+        public required string LastName { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public int TeamId { get; set; }
+
+        public string ?TeamName { get; set; }
+    }
+}
